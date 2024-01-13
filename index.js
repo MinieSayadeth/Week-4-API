32d5f227970a95aaad552b622e7620d7b2f7d621
Minie <minieblogs23@gmail.com>
2024-01-13 17:26:38 -0500

    Finished errors on search

--- index.js
+++ index.js
@@ -1,35 +1,52 @@
-const mealsListEl = document.querySelector('.meals')
+const mealsListEl = document.querySelector(".meals");
 
 async function main() {
-  const meals = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast`)
-  const mealsData = await meals.json()
-  mealsListEl.innerHTML = mealsData.meals.map((meals) => mealsHTML(meals)).join('')
+  const meals = await fetch(
+    `https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast`
+  );
+  const mealsData = await meals.json();
+  mealsListEl.innerHTML = mealsData.meals
+    .map((meals) => mealsHTML(meals))
+    .join("");
 }
 
-main()
-
+main();
 
 function mealsHTML(meals) {
-  return  `<div class="meal__container loading">
+  return `<div class="meal__container">
   <h3 class="meal__title">${meals.strMeal}</h3>          
   <img src="${meals.strMealThumb}" class="meals--img" alt="">
   <p class="meal__Id">
       ${meals.idMeal}
   </p>
-</div>`
+</div>`;
 }
 
 async function onSearch(event) {
-  document.body.classList += ' meals__loading'
-  const mealListValue = (event.target.value)
-  const meals = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${mealListValue}`)
-  const mealsData = await meals.json()
-  mealsListEl.innerHTML = mealsData.meals.slice([0], [6]).map((meals) => mealsHTML(meals)).join('')
-  document.body.classList.remove('meals__loading')
+  document.body.classList += " meals__loading";
+  const mealListValue = event.target.value;
+  const meals = await fetch(
+    `https://www.themealdb.com/api/json/v1/1/filter.php?i=${mealListValue}`
+  );
+  //Search result statement
+  mealsListEl.innerHTML = `<h2>Searching for meals with: ${mealListValue}</h2>`;
+  const mealsData = await meals.json();
+
+  setTimeout(() => {
+    if (mealsData.meals == null) {
+      mealsListEl.innerHTML = `<h2>Oops! Looks like we can't find meals with: ${mealListValue}</h2>`;
+    }
+
+    else if (mealsData == '') {
+      mealsListEl.innerHTML = `<h2>Please enter in an ingredient in the search bar</h2>`;
+    }
+
+    else {
+      mealsListEl.innerHTML = mealsData.meals
+        .slice([0], [6])
+        .map((meals) => mealsHTML(meals))
+        .join("")
+    }
+    document.body.classList.remove("meals__loading");
+  }, 3000)
 }
-
-
-
-
-
-
